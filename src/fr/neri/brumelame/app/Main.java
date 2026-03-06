02e7abd739f1d4749ebfa320595faa90146e39b2
Anthony-Neri <242581876+Anthony-Neri@users.noreply.github.com>
2026-03-06 12:34:42 +0100

    Nettoyage

--- src/fr/neri/brumelame/app/Main.java
+++ src/fr/neri/brumelame/app/Main.java
@@ -5,11 +5,6 @@ import fr.neri.brumelame.game.Game;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-
-    HeroDAO dao = new HeroDAO();
-    Hero hero = dao.find(1);
-
-    System.out.println(hero);
     Game game = new Game();
     game.home();
 
