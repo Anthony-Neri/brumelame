0aae9026170cec68654d2f999e76a8e1150e5c99
Anthony <anthony.ero25@protonmail.com>
2026-03-13 11:32:18 +0100

    Test

--- src/fr/neri/brumelame/app/Main.java
+++ src/fr/neri/brumelame/app/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+
             EquipmentDAO equipmentDAO = new EquipmentDAO();
 
             HeroCreationService heroCreationService = new HeroCreationService(
